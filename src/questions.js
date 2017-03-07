9795e4170b0bf1e3f8fe8a7fedf2712a303f0eaa
Sylvia Sievers <ssievers3@gmail.com>
2017-03-07 17:19:09 -0500

    Add existing file

---
+++ src/questions.js
@@ -0,0 +1,254 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+   
+    "QUESTIONS_EN_US" : [
+
+        {
+            "What eighties band sang a song called Get it on Bang a Gong?": [
+                "PowerStation",
+                "Flock of Seagulls",
+                "Styx",
+                "INXS"
+            ]
+        },
+        {
+            "What eighties band sang a song called The Future’s So Bright I Gotta Wear Shades?": [
+                "Timbuk Three",
+                "Corey Hart",
+                "Flock of Seagulls",
+                "Squeeze"
+            ]
+        },
+        {
+            "What eighties band sang a song called Africa?": [
+                "Toto",
+                "Asia",
+                "Foreigner",
+                "Styx"
+           ]
+        },
+        {
+            "What eighties band sang a song called I Wanna Rock?": [
+                "Twisted Sister",
+                "Kiss",
+                "Quiet Riot",
+                "Ratt"
+           ]
+        },
+        {
+            "What eighties band sang a song called Don’t You Want Me?": [
+                "Human League",
+                "Kiss",
+                "Madness",
+                "Rush"
+           ]
+        },
+        {
+            "Which is not the name of a member of Duran Duran?": [
+                "George",
+                "Nick",
+                "John",
+                "Roger"
+           ]
+        },
+        {
+            "Which Duran Duran song opened with a sample of laughter from the keyboardist’s girlfriend?": [
+                "Hungry like the wolf",
+                "Is there something I should know?",
+                "Girls on Film",
+                "Rio"
+           ]
+        },
+        {
+            "How many singles were released from the Prince album Lovesexy?": [
+                "3 singles",
+                "2 singles",
+                "4 singles",
+                "5 singles"
+           ]
+        },
+        {
+            "Which one of these songs by The Police did not chart in the eighties?": [
+                "Walking on the moon",
+                "Don’t stand so close to me",
+                "Every Breathe you take",
+                "Wrapped around your finger"
+           ]
+        },
+        {
+            "Which Frankie Goes to Hollywood song was banned on UK radio play in nineteen eighty four because of its lyrics?": [
+                "Relax",
+                "Two Tribes",
+                "Welcome to the Pleasuredome",
+                "Rage hard"
+           ]
+        },
+        {
+            "Which Eurythmics song featured the lead singer walking around a stone cottage in a nightgown?": [
+                "Here comes the rain again",
+                "Right by your side",
+                "Would I lie to you",
+                "Sweet Dreams are made of this"
+           ]
+        },
+        {
+            "What was Madonna’s first number one single in the USA?": [
+                "Like a Virgin",
+                "Holiday",
+                "Material Girl",
+                "Borderline"
+           ]
+        },
+        {
+            "Which of these hits was not actually from the eighties?": [
+                "Roxanne by The Police",
+                "All out of Love by Air Supply",
+                "Waiting for a Girl Like You by Foreigner",
+                "Don’t Stop Believing by Journey"
+           ]
+        },
+        {
+            "Which album sold the most copies in the USA?": [
+                "Appetite for Destruction by Guns N’ Roses",
+                "Slippery When Wet by Bon Jovi",
+                "Hysteria by Def Leopard",
+                "Born in the USA by Bruce Springsteen"
+            ]
+        },
+        {
+            "Which Hall and Oats hit is NOT from the eighties?": [
+                "Rich Girl",
+                "I can’t go for that no can do",
+                "Kiss on my list",
+                "Maneater"
+           ]
+        },
+        {
+            "The pop group WHAM! was made up of George Michael and who else?": [
+                "Andrew Ridgley",
+                "Andrew Garfield",
+                "Andrew McCarthy",
+                "David Stewart"
+            ]
+        },
+        {
+            "Which song was NOT a number one hit for George Michael in the USA?": [
+                "I Want Your Sex",
+                "Faith",
+                "Careless Whisper",
+                "Father Figure"
+           ]
+        },
+        {
+            "Which of these hits was not from a soundtrack?": [
+                "Take my Breath Away by Berlin",
+                "Against all Odds by Phil Collins",
+                "Dance Hall Days by Wang Chung",
+                "The Power of Love by Huey Lewis and the News"
+           ]
+        },
+        {
+            "Which eighties movie was the highest grossing film of the decade?": [
+                "ET The Extraterrestrial",
+                "Star Wars Empire Strikes back",
+                "Raiders of the Lost Ark",
+                "Top Gun"
+           ]
+        },
+        {
+            "What eighties hit film included the line 'We came. We Saw. We kicked its ass?": [
+                "Ghost Busters",
+                "Top Gun",
+                "Ferris Bueller’s day off",
+                "Beverly Hills Cop"
+           ]
+        },
+        {
+            "What eighties hit film included the line Life moves pretty fast. If you don’t stop and look around once in a while, you could miss it?": [
+                "Ferris Bueller’s day off",
+                "Ghost Busters",
+                "Top Gun",
+                "Beverly Hills Cop"
+           ]
+        },
+        {
+            "What is Madonna’s real name?": [
+                "Madonna Louise Ciccone",
+                "Maria Lourdes Ciccone",
+                "Madonna Lourdes Ciccone",
+                "Louise Marie Ciccone"
+           ]
+        },
+        {
+            "The hit song Say Say Say was performed by which two artists?": [
+                "Paul McCartney and Michael Jackson",
+                "Stevie Wonder and Paul McCartney",
+                "Stevie Wonder and Michael Jackson",
+                "Paul McCartney and Prince"
+            ]
+        },
+        {
+            "The hit song Ebony and Ivory was performed by which two artists?": [
+                "Stevie Wonder and Paul McCartney",
+                "Paul McCartney and Michael Jackson",
+                "Stevie Wonder and Michael Jackson",
+                "Paul McCartney and Prince"
+           ]
+        },
+        {
+            "What model of car was used as a time machine in the Back to the Future movies?": [
+                "DeLorean",
+                "Porche",
+                "Lamborgini",
+                "Corvette",
+           ]
+        },
+        {
+            "Which actor is not typically considered to be part of the Brat Pack?": [
+                "Tom Cruise",
+                "Rob Lowe",
+                "Emilio Estevez",
+                "Andrew McCarthy",
+            ]
+        },
+        {
+            "Who was NOT a founding member of NWA?": [
+                "Ice-T",
+                "Ice Cube",
+                "Eazy-E",
+                "Dr. Dre",
+           ]
+        },
+        {
+            "Which eighties hit single featured Ronnie Spector singing Be My Baby ?": [
+                "Take Me Home Tonight by Eddie Money",
+                "The Stroke by Billie Squier",
+                "Hungry Eyes by Eric Carmen",
+                "She’s Like the Wind by Patrick Swayze"
+           ]
+        },
+        {
+            "In the movie Dirty Dancing what is Baby’s sister named?": [
+                "Lisa",
+                "Laura",
+                "Mary",
+                "Melissa",
+                "Sarah"
+           ]
+        },
+        {
+            "What did Baby carry the night she met Johnny?": [
+                "A watermelon",
+                "A suitcase",
+                "A box",
+                "A bunch of flowers",
+                "A plant"
+            ]
+        }
+    ] 
+};
